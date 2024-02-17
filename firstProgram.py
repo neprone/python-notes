6a43c98e276e8a0a8ad6a5892f7f02a97db29348
Mayank Kumar <113983171+neprone@users.noreply.github.com>
2024-02-17 23:38:36 +0530

    added python notes

---
+++ firstProgram.py
@@ -0,0 +1,47 @@
+# # print("hello world", "hello baby")
+# # print("my age is 21")
+# # print(2+3)
+# # name = "mayank"
+# # print(type(name))
+# # # int
+# # # str
+# # # float
+# # # boolean
+# # # none --> keywords
+# # # python is case sensitive
+# # old = False
+# # a = None
+# # print(type(a))
+
+# # b= 2
+# # c=3
+# # sum = c+b
+# # print(sum)
+
+# # python is a typed language means we dont have to tell the type of the variable 
+# variable declaration is implicit in python
+# a=2
+# b=3
+# c ="@"
+
+# sum = a + b
+# print(sum)
+
+# a=2
+# b=3.2
+# result of a//b is same as floor function  (-5.3 --> -6)
+# c =a//b
+# print(c)
+# print(type(c))
+
+# numerator = - and denominator = + then n%d = +ve
+#  numerator = + and denominator = - then n%d = -ve
+# jo niche rhega wahi ho jayega
+"""hehe bitch"""
+# how to take INPUT in python
+
+# name = input("name :")
+# print(name)
+# age = int(input("price:"))
+# print(age)
+# not>and>or
